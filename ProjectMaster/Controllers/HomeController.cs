44d1588731b5707398f92b9a0e96d4d5771b22a4
Anders K <and3k5@users.noreply.github.com>
2019-01-29 13:45:30 +0100

    Remove default controller actions

--- ProjectMaster/Controllers/HomeController.cs
+++ ProjectMaster/Controllers/HomeController.cs
@@ -66,20 +66,6 @@ namespace ProjectMaster.Controllers
             return RedirectToAction("Index");
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
